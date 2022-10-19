60a1033bd39d93f73b97dfac14889b6aaa4774c5
torstbje <torstbje@uio.no>
2022-10-19 15:36:42 +0200

    Added member method to evaluate external electric field

--- src/PenningTrap.cpp
+++ src/PenningTrap.cpp
@@ -7,7 +7,7 @@
 
 PenningTrap::PenningTrap(double b0, double v0, double d){
     /*
-    Initializer for Particle class
+    Initializer for PenningTrap class
     */
     mag_field_strength = b0;
     potential = v0;
@@ -15,5 +15,18 @@ PenningTrap::PenningTrap(double b0, double v0, double d){
 }
 
 void PenningTrap::add_particle(Particle p_in){
+    /*
+    Adds Particle to the Penning trap
+    */
     particles.push_back(p_in);
+}
+
+arma::vec PenningTrap::external_E_field(arma::vec r){
+    /*
+    Evaluates external electric field at given coordinates
+    */
+
+    arma::vec e_field = r*(potential/(dim*dim));
+    e_field[2] *= -2;
+    return e_field;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,26 @@
 #include "headers/Particle.hpp"
 #include "headers/PenningTrap.hpp"
 
+using namespace arma;
+
 int main(int argc, char const *argv[]){
     /*
     Main cpp file
     */
 
-    double q = 2.;
-    double m = 3.;
+    double q = 1.;
+    double m = 1.;
     double b0 = 9.65e1;
     double v0 = 2.41e6;
     double d = 500;
 
-    arma::vec r = arma::vec(3);
-    arma::vec v = arma::vec(3);
-    for (arma::uword i = 0; i < 3; i++){
-        r[i] = i;
-        v[i] = i;
-    }
+    PenningTrap trap = PenningTrap(b0,v0,d);
+    vec r = vec(3).randn() * 0.1 * trap.dim;  // random initial position
+    vec v = vec(3).randn() * 0.1 * trap.dim;  // random initial velocity
 
     Particle p = Particle(q, m, r, v);
-    PenningTrap trap = PenningTrap(b0,v0,d);
     trap.add_particle(p);
+
 
     return 0;
 }
