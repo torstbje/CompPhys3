c739263bc5d89b17a1801ab2fee400c54fa87dd4
torstbje <torstbje@uio.no>
2022-10-25 03:33:09 +0200

    Separated plots with and without interactions

--- src/plot.py
+++ src/plot.py
@@ -27,29 +27,35 @@ plt_zvt()
 
 def plt_z_phase():
 	# plot z vs v_z
-	filenames = ['textfiles/rk4_int_0.txt']
+	for part_id in range(n_parts):
+		int_data = pd.read_csv('textfiles/rk4_int_' + str(part_id) + '.txt', header=None)
+		plt.plot(int_data[3],int_data[6],label='Particle '+str(part_id))
 
-	for filename in filenames:
-		data = pd.read_csv(filename, header=None)
-		plt.plot(data[3], data[6], label=filename[14:-6])
+	set_paras('z position($\mu m$)','z velocity($m/s$)','z velocity vs z position with interactions','z_phase_int', file_dir, has_label=True)
 
+	for part_id in range(n_parts):
+		int_data = pd.read_csv('textfiles/rk4_non_' + str(part_id) + '.txt', header=None)
+		plt.plot(int_data[3],int_data[6],label='Particle '+str(part_id))
 
-	set_paras('z position($\mu m$)','z velocity($m/s$)','z velocity vs z position $dt=0.01\mu s$','z_phase,dt=0.01.pdf', file_dir, has_label=True)
+	set_paras('z position($\mu m$)','z velocity($m/s$)','z velocity vs z position without interactions','z_phase_non', file_dir, has_label=True)
 
 
 plt_z_phase()
 
 
 def plt_x_phase():
-	# plot z vs v_z
-	filenames = ['textfiles/rk4_int_0.txt']
+	# plot x vs v_x
+	for part_id in range(n_parts):
+		int_data = pd.read_csv('textfiles/rk4_int_' + str(part_id) + '.txt', header=None)
+		plt.plot(int_data[1],int_data[4],label='Particle '+str(part_id))
 
-	for filename in filenames:
-		data = pd.read_csv(filename, header=None)
-		plt.plot(data[1], data[4], label=filename[14:-6])
+	set_paras('x position($\mu m$)','x velocity($m/s$)','x velocity vs x position with interactions','x_phase_int', file_dir, has_label=True)
 
+	for part_id in range(n_parts):
+		int_data = pd.read_csv('textfiles/rk4_non_' + str(part_id) + '.txt', header=None)
+		plt.plot(int_data[1],int_data[4],label='Particle '+str(part_id))
 
-	set_paras('x position($\mu m$)','x velocity($m/s$)','x velocity vs x position $dt=0.01\mu s$','x_phase,dt=0.01.pdf', file_dir, has_label=True)
+	set_paras('x position($\mu m$)','x velocity($m/s$)','x velocity vs x position without interactions','x_phase_non', file_dir, has_label=True)
 
 
 plt_x_phase()
@@ -58,11 +64,16 @@ def plt_xy():
 
 	for part_id in range(n_parts):
 		int_data = pd.read_csv('textfiles/rk4_int_' + str(part_id) + '.txt', header=None)
-		non_data = pd.read_csv('textfiles/rk4_non_' + str(part_id) + '.txt', header=None)
 		plt.plot(int_data[1],int_data[2],label='int'+str(part_id))
+
+	set_paras('x($\mu m$)','y($\mu m$)','trajectory in x-y plane with interactions', 'x-y_int.pdf', file_dir, has_label=True)
+	plt.axis('equal')
+
+	for part_id in range(n_parts):
+		non_data = pd.read_csv('textfiles/rk4_non_' + str(part_id) + '.txt', header=None)
 		plt.plot(non_data[1],non_data[2],label='non'+str(part_id))
 
-	set_paras('x($\mu m$)','y($\mu m$)','trajectory in x-y plane', 'x-y.pdf', file_dir, has_label=True)
+	set_paras('x($\mu m$)','y($\mu m$)','trajectory in x-y plane without interactions', 'x-y_non.pdf', file_dir, has_label=True)
 	plt.axis('equal')
 
 
