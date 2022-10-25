5e390eaa0a6643a9e61095d6912ccb4e1f294d24
torstbje <torstbje@uio.no>
2022-10-25 05:07:06 +0200

    Added total time and number of timesteps as parameters to main

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,27 @@
 using namespace arma;
 
 
-void time_evo(PenningTrap &trap, double dt, double total_t);
+void time_evo(PenningTrap &trap, double total_t, int n_steps);
 
 int main(int argc, char const *argv[]){
     /*
     Main cpp file
     */
 
-    if (argc < 3){
-        std::cout << "Include parameters for method and interactions you doofus";
+    if (argc < 5){
+        std::cout << "\nMissing input parameters! (" << argc - 1 << " parameters was included.) \n"
+        "Necessary parameters:\n"
+        "-Method ('eul'/'rk4') \n"
+        "-Interactions ('0'/'1') \n"
+        "-Total time (in micro seconds) \n"
+        "-Number of time steps (integer) \n";
         return 1;
     }
 
     std::string method = argv[1];
     std::string is_interact = argv[2];
+    double total_t = std::stod(argv[3]);
+    int n = atoi(argv[4]);
 
     double q = 1.;
     double m = 1.;
@@ -31,7 +38,7 @@ int main(int argc, char const *argv[]){
     double d = 500;
 
 
-    // particle 1
+    // Particle 1
     vec r1 = vec(3);
     vec v1 = vec(3);
 
@@ -41,7 +48,7 @@ int main(int argc, char const *argv[]){
     Particle p1 = Particle(q, m, r1, v1);
 
 
-    // particle 2
+    // Particle 2
     vec r2 = vec(3);
     vec v2 = vec(3);
 
@@ -52,31 +59,29 @@ int main(int argc, char const *argv[]){
 
     Particle p2 = Particle(q, m, r2, v2);
 
-
-    // test time evolution for one particle for time: 50μs
-
-    double total_t = 50;
-    double dt = 0.001;
-
+    // Trap
     PenningTrap trap = PenningTrap(b0,v0,d,is_interact,method);
     trap.add_particle(p1);
     trap.add_particle(p2);
-    time_evo(trap, dt, total_t);
+
+    // Evolve system and write to files
+    time_evo(trap, total_t, n);
 
     return 0;
 }
 
 
 // can be moved elsewhere
-void time_evo(PenningTrap& trap, double dt, double total_t) {
+void time_evo(PenningTrap& trap, double total_t, int n_steps) {
     /* evolves the system for the specified time,
      save the given particle's trajectory
      inputs:
      trap: PenningTrap object
-     dt: time step size
      total_t: total time run
+     n_steps: number of steps
      */
 
+    double dt = total_t/n_steps;
     int n_part = trap.particles.size();
     std::vector<std::ofstream> outfiles;
 
@@ -87,7 +92,7 @@ void time_evo(PenningTrap& trap, double dt, double total_t) {
     }
 
     // Update trap and write data to file
-    for (int i=1; i < total_t/dt;i++) {
+    for (int i=1; i < n_steps;i++) {
 
         trap.evolve(dt);
 
