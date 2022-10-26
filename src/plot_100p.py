321d578f4c4d7061f19867da2b77971adcc851a8
torstbje <torstbje@uio.no>
2022-10-26 19:29:43 +0200

    Updated plotting file

--- src/plot_100p.py
+++ src/plot_100p.py
@@ -3,9 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
+if len(sys.argv) < 2:
+    print("You're missing parameters!")
+
+time = sys.argv[1];
+description = "p_frac" + "_" + time
 
 for f in (0.1,0.4,0.7):
-    data = pd.read_csv("textfiles/p_frac_500_" + str(f) + ".txt", header=None)
+    data = pd.read_csv("textfiles/" + description + "_" + str(f) + ".txt", header=None)
     plt.plot(data[0],data[1], label= "f = " + str(f))
 
-set_paras('$\omega_v$','Fraction of Particles in Trap', "p_frac_500", "p_frac_500.pdf", has_label=True)
+set_paras('$\omega_v$','Fraction of Particles in Trap', description, description + ".png", has_label=True)
